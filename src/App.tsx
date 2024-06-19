02ebf6773d6320cac8a9a8406a2d5a7bf9cdfd9e
rj7041103 <jeyjeyelcracv@gmail.com>
2024-06-19 15:27:34 -0400

    Subida de la vista de la autenticacion hecha

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Login from "./views/authVerification/login/Login";
+import register from "./views/authVerification/register/register";
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
+}
+
+export default App;
